ca582260bcccd7d10bf51273429278989f6da32b
unknown <sam_1700@hotmail.com>
2015-11-22 16:25:01 +0100

    module rdy code

--- index.js
+++ index.js
@@ -4,6 +4,14 @@
 
 var ens = {};
 
+/*
+['arr', 'str', 'num', 'boo', 'obj', 'fun'].forEach(function (lib_name) {
+  ens[lib_name] = require('./lib/' + lib_name);
+});
+
+module.exports = ens;
+*/
+
 ens.arr = require('./lib/arr');
 
 ens.str = function (x) {
